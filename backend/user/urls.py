74a1be3d8af76d54b9d26f73df337f0964b11ebf
ChoiGyeongJu <rudwn5142@naver.com>
2022-10-13 18:29:58 +0900

    add kakao social login

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('login/google', GoogleLoginView.as_view()),
-    # path('login/kakao', KakaoLoginView.as_view())
+    path('login/kakao', KakaoLoginView.as_view())
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,7 @@
-from django.http import JsonResponse
-from rest_framework.views import APIView
+from django.http                  import JsonResponse
+from rest_framework.views         import APIView
 from django.conf                  import settings
-from .models import *
+from .models                      import *
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators      import method_decorator
 import jwt, requests, datetime
@@ -13,9 +13,10 @@ class GoogleLoginView(APIView):
         url      = 'https://oauth2.googleapis.com/tokeninfo?id_token=' # 토큰을 이용해서 회원의 정보를 확인하기 위한 gogle api주소
         response = requests.get(url+token) #구글에 id_token을 보내 디코딩 요청
         user     = response.json() # 유저의 정보 저장
+        print(user)
 
         if User.objects.filter(social_login_id = user['sub']).exists(): 
-            user_info           = User.objects.get(social_login_id=user['sub']) 
+            user_info      = User.objects.get(social_login_id=user['sub']) 
             access_token   = jwt.encode({'id': user["sub"], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)}, settings.SECRET_KEY, algorithm='HS256')
             refresh_token  = jwt.encode({'id': user["sub"], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, settings.SECRET_KEY, algorithm='HS256')
 
@@ -37,8 +38,55 @@ class GoogleLoginView(APIView):
                 email           = user.get('email', None)
             )
 
-            access_token  = jwt.encode({'id': new_user_info.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)}, settings.SECRET_KEY, algorithm='HS256')
-            refresh_token = jwt.encode({'id': new_user_info.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, settings.SECRET_KEY, algorithm='HS256')
+            access_token  = jwt.encode({'id': user['sub'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)}, settings.SECRET_KEY, algorithm='HS256')
+            refresh_token = jwt.encode({'id': user['sub'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, settings.SECRET_KEY, algorithm='HS256')
+
+            JWT = {
+                'AccessToken': access_token,
+                'RefreshToken': refresh_token
+            }
+            User.objects.filter(id=new_user_info.id).update(
+                refresh_token = refresh_token
+            )
+
+            return JsonResponse({'message': 'success', 'JWT': JWT}, status=200)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class KakaoLoginView(APIView):
+    def post(self, request):
+        access_token = request.headers["Authorization"]
+        headers      =({'Authorization' : f"Bearer {access_token}"})
+        url          = "https://kapi.kakao.com/v2/user/me" # Authorization을 이용해서 회원의 정보를 확인하기 위한 카카오 API 주소
+        response     = requests.request("POST", url, headers=headers)
+        user         = response.json()
+        print(user)
+
+        if User.objects.filter(social_login_id = user['id']).exists():
+            user_info      = User.objects.get(social_login_id=user['id'])
+            access_token   = jwt.encode({'id': user["id"], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)}, settings.SECRET_KEY, algorithm='HS256')
+            refresh_token  = jwt.encode({'id': user["id"], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, settings.SECRET_KEY, algorithm='HS256')
+
+            JWT = {
+                'AccessToken' : access_token,
+                'RefreshToken': refresh_token
+            }
+            User.objects.filter(id=user_info.id).update(
+                refresh_token = refresh_token
+            )
+
+            return JsonResponse({"message": "success", "JWT": JWT}, status=200)
+
+        else:
+            new_user_info = User.objects.create(
+                social_login_id = user['id'],
+                nickname        = user['properties']['nickname'],
+                social          = SocialPlatform.objects.get(platform ="kakao"),
+                email           = user['properties'].get('email', None)
+            )
+
+            access_token  = jwt.encode({'id': user['id'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)}, settings.SECRET_KEY, algorithm='HS256')
+            refresh_token = jwt.encode({'id': user['id'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, settings.SECRET_KEY, algorithm='HS256')
 
             JWT = {
                 'AccessToken': access_token,
